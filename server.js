21926aef78df4319dafe95fdd5666540d0c36666
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2025-01-30 10:28:01 +0100

    mit post neue Person hinzugefügt

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = 3000;
 
@@ -10,10 +13,15 @@ const data = [
     { name: "Diego", age: 20 },
 ]
 
-
 app.get("/people", (req, res) => {
     res.send(data);
 });
+
+app.post("/people", (req, res) => {
+    data.push(req.body);
+    res.send(req.body);
+})
+
 
 app.listen(port, () => {
     console.log("Server running on port " + port);
